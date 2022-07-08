6bb9f70a0253cd15f6384e80c61e6c909bf26f80
Ilya Sudnik <i.sudnik@rubylabs.com>
2022-07-08 21:27:29 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(name: "SwiftyRSA",
                       dependencies: [
                         // Dependencies declare other packages that this package depends on.
                       ],
-                      targets: [.target(name: "SwiftyRSA"), //,
+                      targets: [.target(name: "SwiftyRSA",
+                                        path: "Source"), //,
                                 //linkerSettings: [.linkedFramework("Security",
                                 // .when(platforms: [.iOS,
                                 // .tvOS,
